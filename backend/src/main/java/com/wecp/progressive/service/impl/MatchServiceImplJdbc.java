9cd284ddfabf3cee362f3812f56ee0ab77cb3928
username <wecp-dummy@gmail.com>
2025-09-27 13:53:46 +0000

    Day 10

--- backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
+++ backend/src/main/java/com/wecp/progressive/service/impl/MatchServiceImplJdbc.java
@@ -1,5 +1,51 @@
 package com.wecp.progressive.service.impl;
 
-public class MatchServiceImplJdbc  {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.wecp.progressive.dao.MatchDAO;
+import com.wecp.progressive.entity.Match;
+import com.wecp.progressive.service.MatchService;
+
+public class MatchServiceImplJdbc implements MatchService {
+
+    private MatchDAO matchDAO;
+
+    public MatchServiceImplJdbc(MatchDAO matchDAO) {
+        this.matchDAO = matchDAO;
+    }
+
+    @Override
+    public Integer addMatch(Match match) throws SQLException {
+       
+        return matchDAO.addMatch(match);
+    }
+
+    @Override
+    public void deleteMatch(int matchId) throws SQLException {
+       
+        matchDAO.deleteMatch(matchId);
+    }
+
+    @Override
+    public List<Match> getAllMatches() throws SQLException {
+    //    List<Match> matches = new ArrayList<>();
+        return matchDAO.getAllMatches();
+    }
+
+    @Override
+    public Match getMatchById(int matchId) throws SQLException{
+       
+        return matchDAO.getMatchById(matchId);
+    }
+
+    @Override
+    public void updateMatch(Match match) throws SQLException {
+       
+        matchDAO.updateMatch(match);
+    }
+
+    
 
 }
